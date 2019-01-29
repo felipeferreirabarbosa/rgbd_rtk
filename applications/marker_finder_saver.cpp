059a695101a743b3ef2a44af83de62c6fd95a025
RODRIGO SARMENTO XAVIER <rodrigosarmentoxx@gmail.com>
2019-01-29 19:02:34 -0300

    Aruco dict added

--- applications/marker_finder_saver.cpp
+++ applications/marker_finder_saver.cpp
@@ -31,7 +31,7 @@ using namespace aruco;
 float marker_size;
 MarkerFinder marker_finder; 
 
-Eigen::Affine3f turtle_trans; //turtlebot pose
+Eigen::Affine3f trans_camera_pose; //turtlebot pose
 int i=0;
 
 //where markers id and poses will be saved
@@ -48,6 +48,8 @@ string listen_id;
 int listen_id_to_int;
 string all_markers_saved; 
 
+
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg); //subscribe to rgb image
 void rosMarkerFinder(cv::Mat rgb); //marker finder
 void listenKeyboardSave(const std_msgs::String::ConstPtr& msg); //listening keyboard input for navigation
@@ -58,19 +60,19 @@ int main(int argc, char** argv){
   string rgb_topic;
   rgb_topic = "camera/rgb/image_raw";
   if(argc != 5 && argc !=4){
-    fprintf(stderr, "Usage: %s <camera calibration file> <marker size> <save where markes will be saved> optional: <rgb_topic> ....bye default : camera/rgb/image_raw \n", argv[0]);
+    fprintf(stderr, "Usage: %s <camera calibration file> <marker size> <aruco dic> <file where markes will be saved> optional: <rgb_topic> ....bye default : camera/rgb/image_raw \n", argv[0]);
     exit(0);
   }
-  if(argc == 4){
+  if(argc == 5){
     printf(" By defult using camera/rgb/image_raw as ros topic\n");  
   }
-   if(argc == 5){
-     rgb_topic = argv[4];
+   if(argc == 6){
+     rgb_topic = argv[5];
   }
   
   marker_size = stof(argv[2]);
   all_markers_saved = argv[3];
-  marker_finder.markerParam(argv[1] , marker_size);
+  marker_finder.markerParam(argv[1] , marker_size, argv[3]);
 
   for(int k=0; k<=254; k++){ //initializing markers
     all_markers[k].id = 0;
@@ -87,7 +89,7 @@ int main(int argc, char** argv){
   image_transport::Subscriber rgb_sub = it.subscribe(rgb_topic, 1, imageCallback);    //subscribing to rgb image
   
   ros::NodeHandle nn;
-  ros::Subscriber odom_sub = nn.subscribe("odom", 1000, odomCallback);
+  ros::Subscriber odom_sub = nn.subscribe("odom", 1, odomCallback);
 
   ros::spin();  //"while true"
 
@@ -109,14 +111,17 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msgRGB){
 }
 
 void rosMarkerFinder(cv::Mat rgb){
-
-  marker_finder.detectMarkers(rgb, turtle_trans);   //Detect and get pose of all aruco markers
+    //for testes
+  Eigen::Affine3f I;
+  I.setIdentity(); 
+  marker_finder.detectMarkers(rgb, trans_camera_pose);   //Detect and get pose of all aruco markers
 
   for (size_t j = 0; j < marker_finder.markers_.size(); j++){
     all_markers[marker_finder.markers_[j].id].id = marker_finder.markers_[j].id;     //save all markers in a vetor 
     all_markers[marker_finder.markers_[j].id].x_pose = marker_finder.marker_poses_[j](0,3); //save marker position 
     all_markers[marker_finder.markers_[j].id].y_pose = marker_finder.marker_poses_[j](1,3);
-    all_markers[marker_finder.markers_[j].id].y_pose = marker_finder.marker_poses_[j](2,3);
+    all_markers[marker_finder.markers_[j].id].z_pose = marker_finder.marker_poses_[j](2,3);
+    cout << all_markers[marker_finder.markers_[0].id].x_pose << " "  << all_markers[marker_finder.markers_[0].id].y_pose << " " <<  all_markers[marker_finder.markers_[0].id].z_pose<<endl;
     marker_finder.markers_[j].draw(rgb, Scalar(0,0,255), 1);   //drawing markers in rgb image
     CvDrawingUtils::draw3dAxis(rgb, marker_finder.markers_[j], marker_finder.camera_params_);
     stringstream ss;
@@ -152,20 +157,36 @@ void listenKeyboardSave(const std_msgs::String::ConstPtr& msg){
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 
-  Eigen::Vector3f v(msg->pose.pose.position.x,msg->pose.pose.position.y,msg->pose.pose.position.z); //subscribing turtlebot pose 
-  Eigen::Quaternionf q(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z); ///subscribing turtlebot orientation pose 
-
-  q.normalize();
-
-  Eigen::Matrix3f R = q.toRotationMatrix();   // convert a quaternion to a 3x3 rotation matrix:
+  Eigen::Vector3f v_robot(msg->pose.pose.position.x,msg->pose.pose.position.y,msg->pose.pose.position.z); //subscribing turtlebot pose 
+  Eigen::Quaternionf q_robot(msg->pose.pose.orientation.w, msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z); ///subscribing turtlebot orientation pose 
   
-  Eigen::Matrix4f Trans;
-  Trans.setIdentity();   // Set to Identity 
-  Trans.block<3,3>(0,0) = R; 
-  Trans.block<3,1>(0,3) = v;
-  turtle_trans = Trans;
 
-  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
-  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+  Eigen::Matrix3f R_robot = q_robot.normalized().toRotationMatrix();   // convert a quaternion to a 3x3 rotation matrix:
+  
+
+  Eigen::Matrix4f robot_pose;
+  robot_pose.setIdentity();   // Set to Identity 
+  robot_pose.block<3,3>(0,0) = R_robot; 
+  robot_pose.block<3,1>(0,3) = v_robot;
+
+  Eigen::Vector3f v_camera(-0.0125, 0.287, 0.087); //subscribing turtlebot pose 
+  Eigen::Quaternionf q_camera(0.5, -0.5, 0.5, -0.5); ///subscribing turtlebot orientation pose 
+
+  q_camera.normalize();
+
+  Eigen::Matrix3f R_camera = q_camera.toRotationMatrix();   // convert a quaternion to a 3x3 rotation matrix:
+
+  Eigen::Matrix4f camera_pose;
+  camera_pose.setIdentity();   // Set to Identity 
+  camera_pose.block<3,3>(0,0) = R_camera; 
+  camera_pose.block<3,1>(0,3) = v_camera;
+
+  //A(Aruco) *  camera_pose * robot_pose.inv 
+  trans_camera_pose = camera_pose*robot_pose.inverse();
+
+  //cout<<trans_camera_pose(0,3)<<" "<<trans_camera_pose(1,3)<<" " << trans_camera_pose(2,3)<<endl;
+  //ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+  //ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
   //ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+  
 }
